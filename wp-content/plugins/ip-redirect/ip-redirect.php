83aaf456c2143a0ceba059c46edfba6f64af138b
rasheedurrehman71@gmail.com <rasheedurrehman71@gmail.com>
2025-02-12 17:46:51 +0500

    Updated IP blocking test

--- wp-content/plugins/ip-redirect/ip-redirect.php
+++ wp-content/plugins/ip-redirect/ip-redirect.php
@@ -3,14 +3,15 @@
 Plugin Name: IP Redirect
 Description: Redirect users if their IP address starts with 77.29.
 Version: 1.0
-Author: Your Name
+Author: RAsheed Ur Rehman
 */
 
 function hs_redirect_by_ip() {
     $user_ip = $_SERVER['REMOTE_ADDR'];
     if (strpos($user_ip, '77.29') === 0) {
-        wp_redirect('https://rasheed-developer.site/'); // Replace with your desired URL
+        wp_redirect('https://rasheed-developer.site/'); 
         exit;
     }
 }
 add_action('init', 'hs_redirect_by_ip');
+
